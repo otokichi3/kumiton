5f38642bf847bbfb0cf2320db0a85a385b77a9d3
YoshizakiKota <yoshizaki@genio.co.jp>
2019-05-21 14:45:15 +0900

    [upd] 参加者一覧のデザイン、Bootstrap のバージョンアップ

--- app/views/header.php
+++ app/views/header.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>くみとん</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/main.css') ?>">
 </head>
 
--- app/views/main.php
+++ app/views/main.php
@@ -8,40 +8,13 @@
         <br>
         <a href="./text.php" target="_blank">テキスト版</a>
         <h3>今回の参加者</h3>
-        <table class="table table-bordered table-hover table-sm table-responsive-md">
-            <thead class="thead-light">
-                <tr>
-                    <th>名前</th>
-                    <th>レベル</th>
-                </tr>
-            </thead>
+        <ul class="list-group list-group-horizontal-md overflow-auto" style="max-height: 300px;">
             <?php foreach ($sanka_list as $name => $level): ?>
-                <tr>
-                    <td><?= $name ?></td>
-                    <td><?= $level ?></td>
-                </tr>
+                <li class="list-group-item"><?= $name ?></li>
             <?php endforeach ?>
-        </table>
+        </ul>
+        <br>
         <h3>試合の組み合わせ</h3>
-        <?php /* foreach ($match_list as $level => $kumi): ?>
-            <?= sprintf('レベル%d(%d)<br><hr>', $level, count($kumi)) ?>
-            <div class="cssgrid">
-                <?php foreach ($kumi as $key => $pair): ?>
-                    <div>
-                        <table class="court">
-                            <tr style="border-bottom: 1px solid black">
-                                <td class="text-center"><?= $pair[0][0] ?></td>
-                                <td class="text-center"><?= $pair[0][1] ?></td>
-                            </tr>
-                            <tr>
-                                <td class="text-center"><?= $pair[1][0] ?></td>
-                                <td class="text-center"><?= $pair[1][1] ?></td>
-                            </tr>
-                        </table>
-                    </div>
-                <?php endforeach ?>
-            </div>
-        <?php endforeach */ ?>
         <div class="cssgrid">
             <?php foreach ($match as $pair): ?>
                 <div>
