3440436b809fe67d98aa52a41bdc839299de6130
wiethkaty <tomorrow_is_another_day11829@gmail.com>
2019-06-03 05:38:16 +0000

    from gcp

--- app/views/header.php
+++ app/views/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>くみとん</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('/css/main.css') ?>">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 	<script src="https://kit.fontawesome.com/8fa3044e7c.js"></script>
 </head>
 
