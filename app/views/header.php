7f966f31ec6c98f5a590726c4fbe07b694cf499f
wiethkaty <tomorrow.is.another.day1829@gmail.com>
2019-06-03 22:49:54 +0900

    [upd] favicon

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>くみとん</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="shuttle.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?= base_url('css/main.css') ?>">
 	<script src="<?= base_url('fontawesome-free-5.8.2-web/js/all.min.js') ?>"></script>
