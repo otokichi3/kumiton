0722600000f4f39f8ab049cab27970ffa5c8cf72
YoshizakiKota <yoshizaki@genio.co.jp>
2019-06-17 13:39:36 +0900

    [upd] 日付挿入

--- app/controllers/Batch.php
+++ app/controllers/Batch.php
@@ -50,7 +50,7 @@ class Batch extends CI_Controller
 		$yesterday   = date('Y-m-d', strtotime('-1 day', time()));
         $artist_info = $this->fm802_model->get_artist_info($yesterday, 5);
 
-		$msg = '';
+		$msg = sprintf('%s のトップ５%s', $yesterday, PHP_EOL);
 		foreach ($artist_info[$yesterday] as $key => $val) {
 			$msg .= sprintf('%s%s: %s', PHP_EOL, $key, $val);
 		}
