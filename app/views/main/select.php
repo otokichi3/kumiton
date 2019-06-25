c69dcdce7c59b4acec6be50ec9f6c1336526a1a6
wiethkaty <tomorrow.is.another.day1829@gmail.com>
2019-06-25 22:57:03 +0900

    [fix] hidden....

--- app/views/main/select.php
+++ app/views/main/select.php
@@ -26,6 +26,28 @@
 				</div>
 				<?php endforeach ?>
 			</div>
+            <table class="table table-bordered table-hover table-sm table-responsive-md" style="display: none;">
+                <caption>メンバーリスト</caption>
+                <thead class="thead-light">
+                    <tr>
+                        <th scope="col">名前</th>
+                        <th scope="col">レベル</th>
+                        <th scope="col">参加<span class="pl-1"><input type="checkbox" name="check_all" id="check_all"></span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($all_member_info as $key => $val): ?>
+                    <?php $checked = (array_search($val['name'], (array)$selected_member) !== FALSE) ? 'checked' : '' ?>
+                    <tr>
+                        <td><?= $val['name'] ?></td>
+                        <td><?= intval($val['level']) ?></td>
+                        <td>
+                            <input type="checkbox" name="selected_member[]" value="<?= $val['name'] ?>" <?= $checked ?>>
+                        </td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
             <button type="button" class="btn btn-primary float-right" id="check_all2">全選択</button>
-            <button type="submit" class="btn btn-primary ">次へ</button>
+            <button type="submit" class="btn btn-primary">次へ</button>
         </form>
